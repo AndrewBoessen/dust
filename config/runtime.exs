a4a09f750b371afe9c7d50480571597b51e5984d
Andrew Boessen <boessena@bc.edu>
2026-03-23 12:52:06 -0400

    update default db paths

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,5 +3,8 @@ import Config
 # Default master key file location
 config :dust_core, :key_path, Path.expand("~/.dust/master.key")
 
-# Default node fitness model file location
-config :dust_core, :fitness_path, Path.expand("~/.dust/fitness_models.bin")
+# Default node fitness model db file location
+config :dust_core, :fitness_path, Path.expand("~/.dust/fitness_models")
+
+# Default mesh shared map db file location
+config :dust_mesh, :data_dir, Path.expand("~/.dust/dust_mesh_db")
